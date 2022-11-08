8f92fc4913c37904a4318a53a78ab83bbe172277
jvrvl <jvr.vll.l.bs.sa@gmail.com>
2022-11-08 12:28:52 -0300

    Version 1:

--- app/src/main/java/com/example/firebasecf/MainActivity.java
+++ app/src/main/java/com/example/firebasecf/MainActivity.java
@@ -26,7 +26,22 @@ public class MainActivity extends AppCompatActivity {
         ValueEventListener alumnoListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                System.out.println("Hubo un cambio");
+                System.out.println("SNAPSHOT--------");
+                System.out.println(snapshot);
+                System.out.println(snapshot.getValue());
+                System.out.println(snapshot.child("2").getValue());
+                for (DataSnapshot alumno:snapshot.getChildren()){
+                    String nombre = alumno.child("nombre").getValue().toString();
+                    String apellido = alumno.child("apellido").getValue().toString();
+                    double nota1 = Double.parseDouble(alumno.child("notas").child("nota1").getValue().toString());
+                    double nota2 = Double.parseDouble(alumno.child("notas").child("nota2").getValue().toString());
+                    double promedio = (nota1+nota2)/2;
+                    System.out.println(nombre+" ");
+                    System.out.println(apellido+" ");
+                    System.out.println(nota1+" ");
+                    System.out.println(nota2+" ");
+                    System.out.println(promedio+" ");
+                }
             }
 
             @Override
